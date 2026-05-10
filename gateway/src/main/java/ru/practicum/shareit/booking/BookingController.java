674b6fcb6811eb13ea7a8feba0316b1fe63c4877
Goshik <bokgosha@yandex.ru>
2026-05-10 23:58:57 +0300

    Remove unnecessary blank lines in BookingController and ItemRequestClient

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -17,7 +17,7 @@ import ru.practicum.shareit.booking.dto.BookingState;
 @Slf4j
 @Validated
 public class BookingController {
-    
+
     private final BookingClient bookingClient;
 
     @GetMapping
--- gateway/src/main/java/ru/practicum/shareit/request/ItemRequestClient.java
+++ gateway/src/main/java/ru/practicum/shareit/request/ItemRequestClient.java
@@ -13,7 +13,7 @@ import ru.practicum.shareit.request.dto.ItemRequestCreateDto;
 
 @Service
 public class ItemRequestClient extends BaseClient {
-    
+
     private static final String API_PREFIX = "/requests";
 
     @Autowired
